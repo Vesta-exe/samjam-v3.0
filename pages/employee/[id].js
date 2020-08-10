a30aa98174cecc4d77df9333b7bc2b1624870eca
Xarva <48779691+Xarva91@users.noreply.github.com>
2020-08-11 04:52:27 +1000

    individual performance register

--- pages/employee/[id].js
+++ pages/employee/[id].js
@@ -2,29 +2,36 @@ import fetch from 'isomorphic-unfetch'
 import EmployeeSummary from '../../components/Employee/EmployeeSummary'
 import EmployeeButtons from '../../components/Employee/EmployeeButtons'
 import EmployeeCertificates from '../../components/Employee/EmployeeCertificates'
-//import EmployeeRegister from '../../components/Employee/EmployeeRegister'
+import EmployeeRegister from '../../components/Employee/EmployeeRegister'
 //import EmployeeTraining from '../../components/Employee/EmployeeTraining'
 import baseUrl from '../../utils/baseUrl'
 
 //TODO: Add Performance Segment
 //TODO: Add Training Segment
 
-function Employee({employee}) {
+function Employee({employee, performances}) {
     return (
         <>
             <EmployeeSummary employee = {employee}/>
             <EmployeeCertificates employee = {employee}/>
             <br/>
             <EmployeeButtons employee = {employee}/>
-            {/* <EmployeeUpdate {...employee}/> */}
+            <br/>
+            <EmployeeRegister employee = {employee} performances = {performances}/>
         </>
     )
 }
 
 Employee.getInitialProps = async ({query: {id}}) => {
     const employee = await fetch(`${baseUrl}/api/employees/${id}`)
+    const performance = await fetch(`${baseUrl}/api/performance`)
     const {employeeData} = await employee.json()
-    return {employee: employeeData}
+    const {performanceData} = await performance.json()
+
+    return {
+        employee: employeeData,
+        performances: performanceData
+    }
 }
 
 export default Employee

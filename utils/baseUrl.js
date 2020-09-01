0622971fdcb29ae60df660b930629d2ed7b26b71
Xarva <48779691+Xarva91@users.noreply.github.com>
2020-09-02 09:44:08 +1000

    baseUrl Change

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production"
-    ? 'https://deployment-url.nw.sh'
+    ? 'https://samjam.vercel.app'
     : 'http://localhost:3000'
 
 export default baseUrl

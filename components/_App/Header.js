f598a15bfca624a3fab6eada0b4d6121a54b9e2a
Xarva <48779691+Xarva91@users.noreply.github.com>
2020-10-27 20:36:12 +1100

    secruity update v1.4.5

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -152,7 +152,7 @@ function Header() {
                 ) : null}
 
                 <div className='version' >
-                    Version 1.4.4
+                    Version 1.4.5
                 </div>
 
             </Container>
